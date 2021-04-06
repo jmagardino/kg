e036f86ca6147792b60ce1739f0feaf50b62aef8
Joe Magardino <jmagardino@gmail.com>
2021-04-05 19:32:58 -0600

    Adding a n address field to a customer

--- test/kg/accounts_test.exs
+++ test/kg/accounts_test.exs
@@ -91,6 +91,17 @@ defmodule Kg.AccountsTest do
       assert is_nil(user.confirmed_at)
       assert is_nil(user.password)
     end
+
+    test "registers users with an address" do
+      email = unique_user_email()
+      address = "38 Falconers Way"
+      {:ok, user} = Accounts.register_user(%{email: email, password: valid_user_password(), address: address})
+      assert user.email == email
+      assert user.address == address
+      assert is_binary(user.hashed_password)
+      assert is_nil(user.confirmed_at)
+      assert is_nil(user.password)
+    end
   end
 
   describe "make_admin_user/1" do
