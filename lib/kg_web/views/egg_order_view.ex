d4dd1ba59a8d0dc98d804629d3a82726dec3903a
Joseph Magardino <jmagardino@gmail.com>
2021-04-09 21:17:23 -0400

    Pending and Completed Egg Orders (#16)
    * adding a pending and completed order portion for eggs
    
    * redesigned orders page.
    
    * minor table heading adjustment.
    
    * minor action dropdown change.
    
    Co-authored-by: Josh Cobert <jdcobert@gmail.com>

--- lib/kg_web/views/egg_order_view.ex
+++ lib/kg_web/views/egg_order_view.ex
@@ -1,3 +1,7 @@
 defmodule KgWeb.EggOrderView do
   use KgWeb, :view
+
+  def completed_egg_orders(egg_orders), do: Enum.reject(egg_orders, fn egg_order -> egg_order.completed_at == nil end)
+
+  def pending_egg_orders(egg_orders), do: Enum.filter(egg_orders, fn egg_order -> egg_order.completed_at == nil end)
 end
