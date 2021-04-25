65edf22585182110270b7b8660ed06fa346cebbb
Josh Cobert <35186740+jcobert@users.noreply.github.com>
2021-04-25 17:46:42 -0400

    Jc/fix carousel animatin (#20)
    * fixed animations.js
    
    * fixed animations.js
    
    Co-authored-by: Joe Magardino <jmagardino@gmail.com>

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,27 +1,30 @@
+var $ = require('jquery')
 
 function carouselNormalization() {
     var items = $('#testimonial-carousel .carousel-item'), //grab all slides
         heights = [], //create empty array to store height values
         tallest; //create variable to make note of the tallest slide
-    
+        
     if (items.length) {
         function normalizeHeights() {
-            items.each(function() { //add heights to array
-                heights.push($(this).height()); 
+            items.each(function () { //add heights to array
+                heights.push($(this).height());
             });
             tallest = Math.max.apply(null, heights); //cache largest value
-            items.each(function() {
-                $(this).css('min-height',tallest + 'px');
+            items.each(function () {
+                $(this).css('min-height', tallest + 'px');
             });
         };
         normalizeHeights();
-    
+
         $(window).on('resize orientationchange', function () {
             tallest = 0, heights.length = 0; //reset vars
-            items.each(function() {
-                $(this).css('min-height','0'); //reset min-height
-            }); 
+            items.each(function () {
+                $(this).css('min-height', '0'); //reset min-height
+            });
             normalizeHeights(); //run it again 
         });
     }
-    }
+}
+
+carouselNormalization();
